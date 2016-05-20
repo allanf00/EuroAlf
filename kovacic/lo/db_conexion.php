e75d68cd388714a50b5f69100a1ba323261bd892
Alfonso <allanf00@estudiantes.unileon.es>
2016-05-20 16:38:49 +0000

    Removed folder from repository

--- kovacic/lo/db_conexion.php
+++
@@ -1,8 +0,0 @@
-<?PHP
-    //Incluir siempre mediante include_once
-    //Constantes pra la conexión a la DB
-    define("HOST", 'localhost');
-    define("USER_DB", 'root');
-    define("PASSWORD_DB", '');
-    define("DB", 'euro'); //Cambiar a la DB definitiva euro2016
-?>
